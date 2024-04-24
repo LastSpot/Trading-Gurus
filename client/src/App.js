e9c64f0802f564721bdc1c59e5edcb32d8832bd4
johncle <90406181+johncle@users.noreply.github.com>
2024-04-24 19:15:56 -0400

    added tradingview lightweight-charts

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import ChartComponent from "./TradingViewChart";
+
+const initialData = [
+    { time: "2018-12-22", value: 32.51 },
+    { time: "2018-12-23", value: 31.11 },
+    { time: "2018-12-24", value: 27.02 },
+    { time: "2018-12-25", value: 27.32 },
+    { time: "2018-12-26", value: 25.17 },
+    { time: "2018-12-27", value: 28.89 },
+    { time: "2018-12-28", value: 25.46 },
+    { time: "2018-12-29", value: 23.92 },
+    { time: "2018-12-30", value: 22.68 },
+    { time: "2018-12-31", value: 22.67 },
+];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            {/* <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header> */}
+            <ChartComponent data={initialData}></ChartComponent>
+        </div>
+    );
 }
 
 export default App;
